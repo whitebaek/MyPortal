5b53869c4f6ccc4b2c19e0baa31a6c67b22a1670
Michael <whitebaek@gmail.com>
2014-08-25 21:52:04 +1000

    Add anno chat using signalr

---
+++ MyPortal/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MyPortal.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            Clients.Caller.broadcastMessage("Me:" + message);
+            Clients.Others.broadcastMessage("Anon:" + message);
+        }
+    }
+}
